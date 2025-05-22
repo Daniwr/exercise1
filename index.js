a22daba5915d19b13f6227697d0e6c0d08d1caa9
Daniwr <daniwr.ttan@gmail.com>
2025-05-22 13:46:51 -0600

    get_unia

--- index.js
+++ index.js
@@ -11,8 +11,8 @@ app.get("/temperatura", (req, res) => {
   res.json({ valor: `${10} °C`, timestamp: new Date().toISOString()});
 });
 
-app.get("/data", (req, res) => {
-  res.json({ valor: `${34} °C`, timestamp: new Date().toISOString()}, { nombre: "Doustan", apellido: "Rodriguez", edad: "21"}, { universidad: "Universidad Tecnologica de la Laguna"});
+app.get("/uni", (req, res) => {
+  res.json({ universidad: "Universidad Tecnologica de la Laguna"});
 });
 
 app.listen(PORT, () => {
