fa27f77d182797d5e35f9cf2276f34b4b757c9fc
Daniwr <daniwr.ttan@gmail.com>
2025-05-22 13:39:41 -0600

    get_data

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ app.get("/temperatura", (req, res) => {
   res.json({ valor: `${10} °C`, timestamp: new Date().toISOString()});
 });
 
-app.get("/velocidad", (req, res) => {
+app.get("/data", (req, res) => {
   res.json({ valor: `${34} °C`, timestamp: new Date().toISOString()});
   res.json({ nombre: "Doustan", apellido: "Rodriguez", edad: "21"});
   res.json({ universidad: "Universidad Tecnologica de la Laguna"});
